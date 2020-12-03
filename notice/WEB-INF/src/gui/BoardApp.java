795a9eb4f85a199c7f5bf6174cf1a00312ab9b68
YOUNG_JU\silve <silverkay92@gmail.com>
2020-12-03 21:48:37 +0900

    20201202

---
+++ notice/WEB-INF/src/gui/BoardApp.java
@@ -0,0 +1,11 @@
+/*JavaSE의 그래픽 프로그래밍으로 어제 만든 게시판을 구현해 본다 
+즉, 스윙버전의 게시판 제작*/
+
+package gui;
+import javax.swing;
+import java.awt.*;
+import java.event.*;
+
+public class BoardApp extends JFrame{
+
+}
